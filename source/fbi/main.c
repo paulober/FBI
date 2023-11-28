ea5d30be31fb7d54ee25f85ae1b34fd9da6244bc
paulober <44974737+paulober@users.noreply.github.com>
2023-11-28 10:01:04 +0100

    Update README and licensing for the fork
    Signed-off-by: paulober <44974737+paulober@users.noreply.github.com>

--- source/fbi/main.c
+++ source/fbi/main.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015-2019 Steveice10
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE-MIT file in the root directory of this source tree.
+ *
+ * Copyright (c) 2023 paulober
+ * All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include <sys/iosupport.h>
 #include <malloc.h>
 
@@ -12,7 +27,7 @@
 #include "section.h"
 #include "task/uitask.h"
 
-#define CURRENT_KPROCESS (*(void**) 0xFFFF9004)
+#define CURRENT_KPROCESS (*(void **)0xFFFF9004)
 
 #define KPROCESS_PID_OFFSET_OLD (0xB4)
 #define KPROCESS_PID_OFFSET_NEW (0xBC)
@@ -23,16 +38,17 @@ static u32 old_pid = 0;
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wreturn-type"
-static __attribute__((naked)) Result svcGlobalBackdoor(s32 (*callback)()) {
+static __attribute__((naked)) Result svcGlobalBackdoor(s32 (*callback)())
+{
     asm volatile(
-            "svc 0x30\n"
-            "bx lr"
-    );
+        "svc 0x30\n"
+        "bx lr");
 }
 #pragma GCC diagnostic pop
 
-static s32 patch_pid_kernel() {
-    u32 *pidPtr = (u32*) (CURRENT_KPROCESS + (n3ds ? KPROCESS_PID_OFFSET_NEW : KPROCESS_PID_OFFSET_OLD));
+static s32 patch_pid_kernel()
+{
+    u32 *pidPtr = (u32 *)(CURRENT_KPROCESS + (n3ds ? KPROCESS_PID_OFFSET_NEW : KPROCESS_PID_OFFSET_OLD));
 
     old_pid = *pidPtr;
     *pidPtr = 0;
@@ -41,8 +57,9 @@ static s32 patch_pid_kernel() {
     return 0;
 }
 
-static s32 restore_pid_kernel() {
-    u32 *pidPtr = (u32*) (CURRENT_KPROCESS + (n3ds ? KPROCESS_PID_OFFSET_NEW : KPROCESS_PID_OFFSET_OLD));
+static s32 restore_pid_kernel()
+{
+    u32 *pidPtr = (u32 *)(CURRENT_KPROCESS + (n3ds ? KPROCESS_PID_OFFSET_NEW : KPROCESS_PID_OFFSET_OLD));
 
     *pidPtr = old_pid;
 
@@ -50,7 +67,8 @@ static s32 restore_pid_kernel() {
     return 0;
 }
 
-static bool attempt_patch_pid() {
+static bool attempt_patch_pid()
+{
     backdoor_ran = false;
     APT_CheckNew3DS(&n3ds);
 
@@ -62,14 +80,17 @@ static bool attempt_patch_pid() {
     return backdoor_ran;
 }
 
-static void (*exit_funcs[16])()= {NULL};
+static void (*exit_funcs[16])() = {NULL};
 static u32 exit_func_count = 0;
 
-static void* soc_buffer = NULL;
+static void *soc_buffer = NULL;
 
-void cleanup_services() {
-    for(u32 i = 0; i < exit_func_count; i++) {
-        if(exit_funcs[i] != NULL) {
+void cleanup_services()
+{
+    for (u32 i = 0; i < exit_func_count; i++)
+    {
+        if (exit_funcs[i] != NULL)
+        {
             exit_funcs[i]();
             exit_funcs[i] = NULL;
         }
@@ -77,7 +98,8 @@ void cleanup_services() {
 
     exit_func_count = 0;
 
-    if(soc_buffer != NULL) {
+    if (soc_buffer != NULL)
+    {
         free(soc_buffer);
         soc_buffer = NULL;
     }
@@ -85,28 +107,29 @@ void cleanup_services() {
 
 #define INIT_SERVICE(initStatement, exitFunc) (R_SUCCEEDED(res = (initStatement)) && (exit_funcs[exit_func_count++] = (exitFunc)))
 
-Result init_services() {
+Result init_services()
+{
     Result res = 0;
 
     soc_buffer = memalign(0x1000, 0x100000);
-    if(soc_buffer != NULL) {
+    if (soc_buffer != NULL)
+    {
         Handle tempAM = 0;
-        if(R_SUCCEEDED(res = srvGetServiceHandle(&tempAM, "am:net"))) {
+        if (R_SUCCEEDED(res = srvGetServiceHandle(&tempAM, "am:net")))
+        {
             svcCloseHandle(tempAM);
 
-            if(INIT_SERVICE(amInit(), amExit)
-               && INIT_SERVICE(cfguInit(), cfguExit)
-               && INIT_SERVICE(acInit(), acExit)
-               && INIT_SERVICE(ptmuInit(), ptmuExit)
-               && INIT_SERVICE(pxiDevInit(), pxiDevExit)
-               && INIT_SERVICE(httpcInit(0), httpcExit)
-               && INIT_SERVICE(socInit(soc_buffer, 0x100000), (void (*)()) socExit));
+            if (INIT_SERVICE(amInit(), amExit) && INIT_SERVICE(cfguInit(), cfguExit) && INIT_SERVICE(acInit(), acExit) && INIT_SERVICE(ptmuInit(), ptmuExit) && INIT_SERVICE(pxiDevInit(), pxiDevExit) && INIT_SERVICE(httpcInit(0), httpcExit) && INIT_SERVICE(socInit(soc_buffer, 0x100000), (void (*)())socExit))
+                ;
         }
-    } else {
+    }
+    else
+    {
         res = R_APP_OUT_OF_MEMORY;
     }
 
-    if(R_FAILED(res)) {
+    if (R_FAILED(res))
+    {
         cleanup_services();
     }
 
@@ -115,23 +138,28 @@ Result init_services() {
 
 static u32 old_time_limit = UINT32_MAX;
 
-void init() {
+void init()
+{
     gfxInitDefault();
 
     Result romfsRes = romfsInit();
-    if(R_FAILED(romfsRes)) {
+    if (R_FAILED(romfsRes))
+    {
         error_panic("Failed to mount RomFS: %08lX", romfsRes);
         return;
     }
 
-    if(R_FAILED(init_services())) {
-        if(!attempt_patch_pid()) {
+    if (R_FAILED(init_services()))
+    {
+        if (!attempt_patch_pid())
+        {
             error_panic("Kernel backdoor not installed.\nPlease run a kernel exploit and try again.");
             return;
         }
 
         Result initRes = init_services();
-        if(R_FAILED(initRes)) {
+        if (R_FAILED(initRes))
+        {
             error_panic("Failed to initialize services: %08lX", initRes);
             return;
         }
@@ -141,7 +169,8 @@ void init() {
 
     APT_GetAppCpuTimeLimit(&old_time_limit);
     Result cpuRes = APT_SetAppCpuTimeLimit(30);
-    if(R_FAILED(cpuRes)) {
+    if (R_FAILED(cpuRes))
+    {
         error_panic("Failed to set syscore CPU time limit: %08lX", cpuRes);
         return;
     }
@@ -153,14 +182,16 @@ void init() {
     task_init();
 }
 
-void cleanup() {
+void cleanup()
+{
     clipboard_clear();
 
     task_exit();
     ui_exit();
     screen_exit();
 
-    if(old_time_limit != UINT32_MAX) {
+    if (old_time_limit != UINT32_MAX)
+    {
         APT_SetAppCpuTimeLimit(old_time_limit);
     }
 
@@ -173,15 +204,18 @@ void cleanup() {
     gfxExit();
 }
 
-int main(int argc, const char* argv[]) {
-    if(argc > 0 && envIsHomebrew()) {
+int main(int argc, const char *argv[])
+{
+    if (argc > 0 && envIsHomebrew())
+    {
         fs_set_3dsx_path(argv[0]);
     }
 
     init();
 
     mainmenu_open();
-    while(aptMainLoop() && ui_update());
+    while (aptMainLoop() && ui_update())
+        ;
 
     cleanup();
 
--- source/libs/stb_image/stb_image.h
+++ source/libs/stb_image/stb_image.h
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2015-2019 Steveice10
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE-MIT file in the root directory of this source tree.
+ *
+ * Copyright (c) 2023 paulober
+ * All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 /* stb_image - v2.19 - public domain image loader - http://nothings.org/stb
                                   no warranty implied; use at your own risk
 
